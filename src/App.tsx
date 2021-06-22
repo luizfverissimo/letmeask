bba8e6ba3eb5b8b9620cd86759aabac10338cd67
Luiz Fernando Verissimo <luizfverissimo@gmail.com>
2021-06-22 13:55:23 -0300

    aula 02 DONE

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import { AuthContextProvider } from './context/AuthContext';
+import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
+
 function App() {
   return (
-    <h1>Olá Mundão</h1>
+    <AuthContextProvider>
+      <BrowserRouter>
+        <Route path='/' exact component={Home} />
+        <Route path='/rooms/new' component={NewRoom} />
+      </BrowserRouter>
+    </AuthContextProvider>
   );
 }
 
